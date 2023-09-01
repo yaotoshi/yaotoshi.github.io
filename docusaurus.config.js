48d90ccd3e8defaa7b475ea90c42462b3362dfdd
Yaotoshi Nakamoto <yaotoshi.x@ud.me>
2023-09-02 02:53:21 +0700

    test workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'yaotoshi', // Usually your GitHub org/user name.
-  projectName: 'whitepaper', // Usually your repo name.
+  projectName: 'yaotoshi.github.io', // Usually your repo name.
   // deploymentBranch: 'deployment',
   trailingSlash: false,
 
